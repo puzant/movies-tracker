0af2db54ad50263316cca8538062dea58d912751
Puzant <PBAKJEJIAN@hotmail.com>
2023-11-28 10:30:59 +0200

    add custom paginated query hook

---
+++ src/hooks/usePaginatedQuery.ts
@@ -0,0 +1,24 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+
+interface IPage {
+  data: {
+    page: number 
+    total_pages: number
+  }
+}
+
+const useInfiniteMovieQuery = (queryKey, queryFn) => {
+  return useInfiniteQuery({
+    queryKey,
+    queryFn,
+    initialPageParam: 1,
+    getNextPageParam: (lastPage: IPage) => {
+      if (lastPage.data.page < lastPage.data.total_pages) {
+        return lastPage.data.page + 1;
+      }
+      return undefined;
+    },
+  }) 
+}
+
+export default useInfiniteMovieQuery
