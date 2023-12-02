6f7880e6de396859e445ed6d5ebef3a77e3e0922
Puzant <PBAKJEJIAN@hotmail.com>
2023-12-02 19:32:10 +0200

    use luxon adapter

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import { BrowserRouter } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
-import 'moment/locale/de';
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterLuxon } from "@mui/x-date-pickers/AdapterLuxon";
 
-import AppRoutes from '@/appRoutes';
-import { Navbar } from '@/components';
+import AppRoutes from "@/appRoutes";
+import { Navbar } from "@/components";
 
 function App() {
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <LocalizationProvider dateAdapter={AdapterMoment} adapterLocale="de">
+      <LocalizationProvider dateAdapter={AdapterLuxon} adapterLocale="de">
         <BrowserRouter>
           <Navbar />
           <AppRoutes />
         </BrowserRouter>
       </LocalizationProvider>
     </QueryClientProvider>
-  )
+  );
 }
 
-export default App
+export default App;
