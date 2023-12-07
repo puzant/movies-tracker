09cee8c12fcbe30b745bf9201e6d5a55bd548ca7
Puzant <PBAKJEJIAN@hotmail.com>
2023-12-07 19:19:01 +0200

    use HOC in app routes

--- src/appRoutes/index.tsx
+++ src/appRoutes/index.tsx
@@ -1,16 +1,31 @@
-import { Routes, Route } from 'react-router-dom';
-import { Home, Login, MovieDetails, UpcomingMovies, SearchResults } from '@/pages';
+import { Routes, Route } from "react-router-dom";
+
+import withApiFunctions from "@/HOC/withApiFunctions";
+
+import {
+  Home,
+  Login,
+  MovieDetails,
+  UpcomingMovies,
+  SearchResults,
+} from "@/pages";
+
+const HomeWithApi = withApiFunctions(Home);
+const UpcomingMoviesWithApi = withApiFunctions(UpcomingMovies);
+const SearchResultsWithApi = withApiFunctions(SearchResults);
+const MovieDetailsWithApi = withApiFunctions(MovieDetails);
+const LoginWithApi = withApiFunctions(Login);
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/upcoming" element={<UpcomingMovies />} />
-      <Route path="/search-results" element={<SearchResults />} />
-      <Route path="/movie/:movieId" element={<MovieDetails />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<HomeWithApi />} />
+      <Route path="/upcoming" element={<UpcomingMoviesWithApi />} />
+      <Route path="/search-results" element={<SearchResultsWithApi />} />
+      <Route path="/movie/:movieId" element={<MovieDetailsWithApi />} />
+      <Route path="/login" element={<LoginWithApi />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;
