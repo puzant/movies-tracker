e5e3e7613ad763cd593eb7a7e4584189b3161804
Puzant <PBAKJEJIAN@hotmail.com>
2023-12-14 15:51:14 +0200

    add types

--- src/components/actor.tsx
+++ src/components/actor.tsx
@@ -1,11 +1,20 @@
-export const Actor = ({ actor }) => {
+import { ICast } from "@/interfaces";
+
+export const Actor = ({ actor }: { actor: ICast }) => {
   return (
     <div className="border shadow-md flex-1 rounded-xl">
-      <img className="w-full h-[200px] rounded-t-md object-cover" src={`https://image.tmdb.org/t/p/w185/${actor.profile_path}`} />
+      <img
+        className="w-full h-[200px] rounded-t-md object-cover"
+        src={`${
+          actor.profile_path !== null
+            ? `https://image.tmdb.org/t/p/w185/${actor.profile_path}`
+            : "https://d3uscstcbhvk7k.cloudfront.net/static/images/slider-placeholder-2x.png"
+        }`}
+      />
       <div className="p-4">
         <span className="block font-bold">{actor.name}</span>
         <span className="w-[130px] block break-words">{actor.character}</span>
       </div>
     </div>
-  )
-}
+  );
+};
