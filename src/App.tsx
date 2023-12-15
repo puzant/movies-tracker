c0458ff471fa5d1f895f532486908dbe967bf4ad
Puzant <PBAKJEJIAN@hotmail.com>
2023-12-15 14:16:35 +0200

    implement internationalization :books:

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
@@ -7,8 +9,29 @@ import { AdapterLuxon } from "@mui/x-date-pickers/AdapterLuxon";
 import AppRoutes from "@/appRoutes";
 import { Navbar } from "@/components";
 
+import en from "@/locale/en.json";
+import fr from "@/locale/fr.json";
+import ar from "@/locale/ar.json";
+
 function App() {
   const queryClient = new QueryClient();
+
+  i18n.use(initReactI18next).init({
+    resources: {
+      EN: {
+        translation: en,
+      },
+      FR: {
+        translation: fr,
+      },
+      AR: {
+        translation: ar,
+      },
+    },
+    lng: "EN",
+    fallbackLng: "EN",
+    debug: false,
+  });
 
   return (
     <QueryClientProvider client={queryClient}>
