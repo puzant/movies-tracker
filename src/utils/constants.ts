cb888dc76454ebf4521973a4cb525b658919b7f9
Puzant <PBAKJEJIAN@hotmail.com>
2023-12-20 21:39:40 +0200

    add languages options :construction:

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-import { ISortingOption, IMovie, IGenre } from '@/interfaces';
+import { ISortingOption } from '@/interfaces';
 
 export const sortingOptions: ISortingOption[] = [
   { id: 1, key: 'popularity.desc', name: 'Popularity Descending' },
@@ -7,4 +7,25 @@ export const sortingOptions: ISortingOption[] = [
   { id: 4, key: 'vote_average.asc', name: 'Rating Ascending' },
   { id: 5, key: 'primary_release_date.desc', name: 'Release Date Descending' },
   { id: 6, key: 'primary_release_date.asc', name: 'Release Date Ascending' },
+];
+
+export const languages = [
+  {
+    iso_639_1: "en",
+    english_name: "English",
+    name: "English",
+    flag: "us",
+  },
+  {
+    iso_639_1: "fr",
+    english_name: "French",
+    name: "Français",
+    flag: "fr",
+  },
+  {
+    iso_639_1: "ar",
+    english_name: "Arabic",
+    name: "العربية",
+    flag: "sa",
+  },
 ];
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
+export const isMobile = () => /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
 export const getMovieLanguage = (lang: string) => {
   switch(lang) {
     case "en": 
