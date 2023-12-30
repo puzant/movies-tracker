af4e4d8618a32ca644bfe0879f993ab21183e630
Puzant <PBAKJEJIAN@hotmail.com>
2023-12-30 20:37:17 +0200

    add Attribution

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -11,6 +11,7 @@ import { Filters, FiltersDialog, Movie, LoadingSpinner } from "@/components";
 
 import ErrorIcon from "@mui/icons-material/Error";
 import FilterListIcon from "@mui/icons-material/FilterList";
+import tmdbLogo from "@/assets/tmdbLogo.svg";
 
 export const Home = ({ apiFunctions }: { apiFunctions: IApiFunction }) => {
   const { i18n, t } = useTranslation();
@@ -62,6 +63,14 @@ export const Home = ({ apiFunctions }: { apiFunctions: IApiFunction }) => {
 
   return (
     <div className="mt-8">
+      <div className="border shadow-md rounded-md w-[90%] md:w-[50%] p-5 flex flex-col md:flex-row gap-4 items-center justify-between m-auto my-6 md:my-0">
+        <img className="w-[110px] h-[52px]" src={tmdbLogo} alt="tmdb logo" />
+        <div className="text-center">
+          This product uses the TMDb API but is not endorsed or certified by
+          TMDb.
+        </div>
+      </div>
+
       <div className="px-4 sm:px-8 flex justify-between items-center">
         <span className="text-2xl font-semibold">{t("popular_movies")}</span>
         <div className="block md:hidden" onClick={() => setOpenDialog(true)}>
