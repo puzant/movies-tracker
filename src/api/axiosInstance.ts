dd660252ed91baa33486dc76527dfbb1abe03260
Puzant <PBAKJEJIAN@hotmail.com>
2024-02-10 12:49:02 +0200

    apply formatting

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,16 +1,18 @@
-import axios from 'axios';
+import axios from "axios";
 
-const baseURL = 'https://api.themoviedb.org/3/'
+const baseURL = "https://api.themoviedb.org/3/";
 
 const axiosInstance = axios.create({ baseURL });
 
 axiosInstance.interceptors.request.use(
   (config) => {
-    config.params = { ...config.params, api_key: '63d59f2df02d27e6739533218ba6c9d9' };
+    config.params = {
+      ...config.params,
+      api_key: "63d59f2df02d27e6739533218ba6c9d9",
+    };
     return config;
   },
-  (error) => Promise.reject(error),
+  (error) => Promise.reject(error)
 );
 
 export default axiosInstance;
-
