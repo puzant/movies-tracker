f4acc817bd887624dbae11085ce360e2a2cf7ab4
Puzant <PBAKJEJIAN@hotmail.com>
2024-03-09 13:52:54 +0200

    add describe block

--- src/components/atoms/divider/divider.test.tsx
+++ src/components/atoms/divider/divider.test.tsx
@@ -1,9 +1,11 @@
 import { screen, render } from "@testing-library/react";
 import { Divider } from "./divider";
 
-test("it reneders a divider", () => {
-  render(<Divider />);
-  expect(screen.getByTestId("divider-component")).toHaveClass(
-    "my-2.5 bg-gray-300 w-full h-[1px]"
-  );
+describe("Divider Component", () => {
+  test("it reneders a divider", () => {
+    render(<Divider />);
+    expect(screen.getByTestId("divider-component")).toHaveClass(
+      "my-2.5 bg-gray-300 w-full h-[1px]"
+    );
+  });
 });
--- src/components/molecules/actor/actor.test.tsx
+++ src/components/molecules/actor/actor.test.tsx
@@ -1,44 +1,32 @@
 import { render } from "@testing-library/react";
 import { Actor } from "./actor";
+import { mockActor } from "@/mocks";
 
-const mockActor = {
-  adult: false,
-  gender: 2,
-  id: 1190668,
-  known_for_department: "Acting",
-  name: "Timothée Chalamet",
-  original_name: "Timothée Chalamet",
-  popularity: 186.465,
-  profile_path: "/BE2sdjpgsa2rNTFa66f7upkaOP.jpg",
-  cast_id: 2,
-  character: "Paul Atreides",
-  credit_id: "5e959c45955c6500159f1c98",
-  order: 0,
-};
+describe("Actor Component", () => {
+  test("it renders with correct props", () => {
+    const { getByText } = render(<Actor actor={mockActor} />);
+    expect(getByText(mockActor.name)).toBeInTheDocument();
+    expect(getByText(mockActor.character)).toBeInTheDocument();
+  });
 
-test("it renders with correct props", () => {
-  const { getByText } = render(<Actor actor={mockActor} />);
-  expect(getByText(mockActor.name)).toBeInTheDocument();
-  expect(getByText(mockActor.character)).toBeInTheDocument();
-});
+  test("renders with correct image URL when profile_path is not null", () => {
+    const { getByRole } = render(<Actor actor={mockActor} />);
+    const imgElement = getByRole("img");
+    expect(imgElement).toBeInTheDocument();
+    expect(imgElement).toHaveAttribute(
+      "src",
+      `https://image.tmdb.org/t/p/w185/${mockActor.profile_path}`
+    );
+  });
 
-it("renders with correct image URL when profile_path is not null", () => {
-  const { getByRole } = render(<Actor actor={mockActor} />);
-  const imgElement = getByRole("img");
-  expect(imgElement).toBeInTheDocument();
-  expect(imgElement).toHaveAttribute(
-    "src",
-    `https://image.tmdb.org/t/p/w185/${mockActor.profile_path}`
-  );
-});
-
-it("renders with fallback image URL when profile_path is null", () => {
-  const actorWithoutImage = { ...mockActor, profile_path: null };
-  const { getByRole } = render(<Actor actor={actorWithoutImage} />);
-  const imgElement = getByRole("img");
-  expect(imgElement).toBeInTheDocument();
-  expect(imgElement).toHaveAttribute(
-    "src",
-    "https://d3uscstcbhvk7k.cloudfront.net/static/images/slider-placeholder-2x.png"
-  );
+  test("renders with fallback image URL when profile_path is null", () => {
+    const actorWithoutImage = { ...mockActor, profile_path: null };
+    const { getByRole } = render(<Actor actor={actorWithoutImage} />);
+    const imgElement = getByRole("img");
+    expect(imgElement).toBeInTheDocument();
+    expect(imgElement).toHaveAttribute(
+      "src",
+      "https://d3uscstcbhvk7k.cloudfront.net/static/images/slider-placeholder-2x.png"
+    );
+  });
 });
