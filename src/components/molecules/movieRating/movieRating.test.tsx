816c9b605c657d04735e5f6a4149d3a915fd0a24
Puzant <PBAKJEJIAN@hotmail.com>
2024-03-09 16:00:54 +0200

    write tests for movie rating

---
+++ src/components/molecules/movieRating/movieRating.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import { MovieRating } from "./movieRating";
+
+describe("Movie Rating Component", () => {
+  test("renders the rating component with correct value", () => {
+    const vote_average = 8.455;
+    render(<MovieRating vote={vote_average} />);
+    const ratingElement = screen.getByRole("img", { name: /4 Stars/i });
+
+    expect(ratingElement.getAttribute("aria-label")).toBe("4 Stars");
+  });
+});
