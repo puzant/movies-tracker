41701792a4061cfc36e346484bc7f7ef9bb60e27
Puzant <PBAKJEJIAN@hotmail.com>
2024-03-09 16:01:00 +0200

    write tests for movie tabs

---
+++ src/components/molecules/movieTabs/movieTabs.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import { MovieTabs } from "./movieTabs";
+
+describe("Movie Tabs Component", () => {
+  test("it renders tabs correctly", () => {
+    render(
+      <MovieTabs favoriteMovies={[]} moviesInWatchlist={[]} ratedMovies={[]} />
+    );
+
+    expect(screen.getByText("favorites")).toBeInTheDocument();
+    expect(screen.getByText("watchlist")).toBeInTheDocument();
+    expect(screen.getByText("rated_movies")).toBeInTheDocument();
+  });
+});
