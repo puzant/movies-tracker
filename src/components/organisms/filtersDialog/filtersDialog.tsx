5c734cdafb0ea59f464bc866f1288ccd1d71cd43
Puzant <PBAKJEJIAN@hotmail.com>
2024-03-10 13:37:26 +0200

    move to new directory

---
+++ src/components/organisms/filtersDialog/filtersDialog.tsx
@@ -0,0 +1,50 @@
+import { useTranslation } from "react-i18next";
+import useUserStore from "@/store/useUserStore";
+import { Filters } from "@/components/organisms";
+
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+
+export const FiltersDialog = ({
+  onClose,
+  openDialog,
+}: {
+  onClose: () => void;
+  openDialog: boolean;
+}) => {
+  const { t } = useTranslation();
+  const { accentColor } = useUserStore();
+
+  return (
+    <Dialog fullScreen onClose={onClose} open={openDialog}>
+      <DialogTitle>
+        <div className="flex justify-between items-center">
+          <span className="font-semibold">{t("sorting_and_filters")}</span>
+          <CloseIcon
+            onClick={onClose}
+            sx={{ color: "#5A5A5A", cursor: "pointer" }}
+          />
+        </div>
+      </DialogTitle>
+
+      <DialogContent>
+        <Filters />
+      </DialogContent>
+
+      <DialogActions>
+        <button
+          onClick={onClose}
+          style={{ background: accentColor }}
+          className="rounded-md w-full px-4 py-2 shadow-md text-white"
+        >
+          {t("apply")}
+        </button>
+      </DialogActions>
+    </Dialog>
+  );
+};
