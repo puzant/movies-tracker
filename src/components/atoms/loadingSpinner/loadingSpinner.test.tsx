cbbe4cdaa34df55cef62317783cf3ea2145be00e
Puzant <PBAKJEJIAN@hotmail.com>
2024-03-15 16:02:34 +0200

    fix loading spinner unit test

--- src/components/atoms/loadingSpinner/loadingSpinner.test.tsx
+++ src/components/atoms/loadingSpinner/loadingSpinner.test.tsx
@@ -2,12 +2,16 @@ import { vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { LoadingSpinner } from "./loadingSpinner";
 
-vi.mock("@/store/useUserStore");
+vi.mock("@/store/useUserStore", () => ({
+  default: vi.fn(() => ({
+    accentColor: "#0177d2",
+  })),
+}));
 
 describe("Loading Spinner Component", () => {
   test("renders the SVG with the correct fill color", () => {
     render(<LoadingSpinner />);
     const svg = screen.getByTestId("loading-spinner-svg");
-    expect(svg).toHaveAttribute("fill", "#0177d2");
+    expect(svg).toHaveStyle("fill: #0177d2");
   });
 });
