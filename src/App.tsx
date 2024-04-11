91c4c3570a682d89c566f7f1906c864394fb0c36
Puzant <PBAKJEJIAN@hotmail.com>
2024-04-11 18:32:27 +0300

    add toast container

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { initReactI18next } from "react-i18next";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import LanguageDetector from "i18next-browser-languagedetector";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterLuxon } from "@mui/x-date-pickers/AdapterLuxon";
@@ -43,6 +45,7 @@ function App() {
       <LocalizationProvider dateAdapter={AdapterLuxon} adapterLocale="de">
         <BrowserRouter>
           <Navbar />
+          <ToastContainer hideProgressBar={false} theme="dark" />
           <AppRoutes />
         </BrowserRouter>
       </LocalizationProvider>
