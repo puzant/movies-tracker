96a5a899a8efec886066f6ba41e2153c705b4655
Puzant <puzant@aktek.io>
2024-04-20 21:28:38 +0300

    delete old files

--- src/components/organisms/filters/filters.test.tsx
+++ src/components/organisms/filters/filters.test.tsx
@@ -7,30 +7,30 @@ import { Filters } from "./filters";
 import useFilters from "@/hooks/useFilters";
 import { sortingOptions } from "@/utils/constants";
 
-// const mockSetGenres = vi.fn();
-// const mockSortBy = vi.fn();
+const mockSetGenres = vi.fn();
+const mockSortBy = vi.fn();
 
-// vi.mock("@/hooks/useFilters", () => ({
-//   default: vi.fn(() => ({
-//     accentColor: "#0177d2",
-//     isFetching: false,
-//     sortBy: sortingOptions[0],
-//     releaseDate: {
-//       start: null,
-//       end: null,
-//     },
-//     selectedGenres: [],
-//     genres: {
-//       genres: [
-//         { id: 1, name: "Action" },
-//         { id: 2, name: "Drama" },
-//       ],
-//     },
-//     t: (key: string) => key,
-//     setSort: mockSortBy,
-//     setGenres: mockSetGenres,
-//   })),
-// }));
+vi.mock("@/hooks/useFilters", () => ({
+  default: vi.fn(() => ({
+    accentColor: "#0177d2",
+    isFetching: false,
+    sortBy: sortingOptions[0],
+    releaseDate: {
+      start: null,
+      end: null,
+    },
+    selectedGenres: [],
+    genres: {
+      genres: [
+        { id: 1, name: "Action" },
+        { id: 2, name: "Drama" },
+      ],
+    },
+    t: (key: string) => key,
+    setSort: mockSortBy,
+    setGenres: mockSetGenres,
+  })),
+}));
 
 const queryClient = new QueryClient();
 
@@ -60,51 +60,51 @@ describe("Filters Component", () => {
       screen.debug();
     });
 
-    // it("renders sorting by menu correctly", () => {
-    //   const { getByText } = render(<Filters />);
-    //   expect(getByText("sorting_options.popularity.desc")).toBeInTheDocument();
-    // });
+    it("renders sorting by menu correctly", () => {
+      const { getByText } = render(<Filters />);
+      expect(getByText("sorting_options.popularity.desc")).toBeInTheDocument();
+    });
 
-    // it("renders date picker component correctly", () => {
-    //   const { getAllByLabelText } = render(<Filters />);
-    //   const buttonElements = getAllByLabelText("Choose date");
+    it("renders date picker component correctly", () => {
+      const { getAllByLabelText } = render(<Filters />);
+      const buttonElements = getAllByLabelText("Choose date");
 
-    //   expect(buttonElements.length).toEqual(2);
-    // });
+      expect(buttonElements.length).toEqual(2);
+    });
 
-    // it("render a list of genres", () => {
-    //   const { getByText } = render(<Filters />);
+    it("render a list of genres", () => {
+      const { getByText } = render(<Filters />);
 
-    //   expect(getByText("Action")).toBeInTheDocument();
-    //   expect(getByText("Drama")).toBeInTheDocument();
-    // });
+      expect(getByText("Action")).toBeInTheDocument();
+      expect(getByText("Drama")).toBeInTheDocument();
+    });
   });
 
-  // describe("Sorting functionlity", () => {
-  //   it("open sorting menu and selects sort by", () => {
-  //     const { setSort } = useFilters();
-  //     const { getByText } = render(<Filters />);
-  //     const sortingMenuButton = getByText("sorting_options.popularity.desc");
+  describe("Sorting functionlity", () => {
+    it("open sorting menu and selects sort by", () => {
+      const { setSort } = useFilters();
+      const { getByText } = render(<Filters />);
+      const sortingMenuButton = getByText("sorting_options.popularity.desc");
 
-  //     fireEvent.click(sortingMenuButton);
-  //     const sortingOptionAsc = getByText("sorting_options.popularity.asc");
-  //     fireEvent.click(sortingOptionAsc);
-  //     expect(setSort).toHaveBeenCalledWith({
-  //       id: 2,
-  //       key: "popularity.asc",
-  //       name: "Popularity Ascending",
-  //     });
-  //   });
-  // });
+      fireEvent.click(sortingMenuButton);
+      const sortingOptionAsc = getByText("sorting_options.popularity.asc");
+      fireEvent.click(sortingOptionAsc);
+      expect(setSort).toHaveBeenCalledWith({
+        id: 2,
+        key: "popularity.asc",
+        name: "Popularity Ascending",
+      });
+    });
+  });
 
-  // describe("Genres selection functionality", () => {
-  //   it("calls setGenres function when genre is clicked", () => {
-  //     const { setGenres } = useFilters();
-  //     const { getByText } = render(<Filters />);
+  describe("Genres selection functionality", () => {
+    it("calls setGenres function when genre is clicked", () => {
+      const { setGenres } = useFilters();
+      const { getByText } = render(<Filters />);
 
-  //     const actionGenreButton = getByText("Action");
-  //     fireEvent.click(actionGenreButton);
-  //     expect(setGenres).toHaveBeenCalledWith({ id: 1, name: "Action" });
-  //   });
-  // });
+      const actionGenreButton = getByText("Action");
+      fireEvent.click(actionGenreButton);
+      expect(setGenres).toHaveBeenCalledWith({ id: 1, name: "Action" });
+    });
+  });
 });
