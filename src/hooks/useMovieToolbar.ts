d38fcecd5f14135f041b0ff67ede83ca140b134b
Puzant <pbakjejian@hotmail.com>
2025-04-24 12:11:29 +0300

    add TS types

--- src/hooks/useMovieToolbar.ts
+++ src/hooks/useMovieToolbar.ts
@@ -11,26 +11,20 @@ import useMovieStore from "@/store/useMovieStore";
 
 const useMovieToolbar = (movieId: number) => {
   const { t } = useTranslation();
-  const { sessionId, accountId, isAuthenticated, accentColor, username, setAccentColor } =
-    useUserStore();
+  const { sessionId, accountId, isAuthenticated, accentColor, username, setAccentColor } = useUserStore();
   const { isFavorite, isInWatchlist, isRated } = useMovieStore();
 
-  const [rating, setRating] = React.useState<any>(isRated);
+  const [rating, setRating] = React.useState<boolean | number | null>(isRated);
   const [showRating, setShowRating] = React.useState<boolean>(false);
 
-  const favoriteTooltip = isAuthenticated
-    ? t("add_movie_to_favorite")
-    : t("login_to_add_movie_to_favorite");
-  const watchListTooltip = isAuthenticated
-    ? t("add_movie_to_watch_list")
-    : t("login_to_add_movie_to_watchlist");
+  const favoriteTooltip = isAuthenticated ? t("add_movie_to_favorite") : t("login_to_add_movie_to_favorite");
+  const watchListTooltip = isAuthenticated ? t("add_movie_to_watch_list") : t("login_to_add_movie_to_watchlist");
   const rateMovieTooltip = isAuthenticated ? t("rate_movie") : t("login_to_rate_movie");
 
-  const { rateMovieMutation } = useRateMovieMutation();
-  const { deleteRatingMutation } = useDeleteMovieRatingMutation();
-  const { setFavoriteMutation, isPending: addingFavoriteLoading } = useFavoriteMovieMutation();
-  const { setMovieWatchListMutation, isPending: addingWatchListLoading } =
-    useMovieWatchListMutation();
+  const { mutateAsync: rateMovieMutation } = useRateMovieMutation();
+  const { mutateAsync: deleteRatingMutation } = useDeleteMovieRatingMutation();
+  const { mutateAsync: setFavoriteMutation, isPending: addingFavoriteLoading } = useFavoriteMovieMutation();
+  const { mutateAsync: setMovieWatchListMutation, isPending: addingWatchListLoading } = useMovieWatchListMutation();
 
   React.useEffect(() => {
     if (isRated !== null) setRating(isRated);
@@ -38,45 +32,33 @@ const useMovieToolbar = (movieId: number) => {
 
   const handleSetMovieToFavorite = () => {
     if (!isAuthenticated) return;
-
-    setFavoriteMutation({
-      accountId,
-      sessionId,
-      id: movieId,
-      favorite: !isFavorite,
-    });
+    setFavoriteMutation({ accountId, sessionId, id: movieId, favorite: !isFavorite });
   };
 
   const handleSetMovieToWatchList = () => {
     if (!isAuthenticated) return;
-
-    setMovieWatchListMutation({
-      accountId,
-      sessionId,
-      id: movieId,
-      isInWatchlist: !isInWatchlist,
-    });
+    setMovieWatchListMutation({ accountId, sessionId, id: movieId, isInWatchlist: !isInWatchlist });
   };
 
-  const handleMovieRating = (ratingValue: number | null) => {
+  const handleMovieRating = async (ratingValue: number | null) => {
     if (!isAuthenticated) return;
     setRating(ratingValue);
 
     if (ratingValue === null) {
-      const deleteResponse: any = deleteRatingMutation({
+      const deleteResponse = await deleteRatingMutation({
         id: movieId,
         sessionId,
       });
 
-      if (deleteResponse.status.success) setRating(ratingValue);
+      if (deleteResponse.success) setRating(ratingValue);
     } else {
-      const ratingResponse: any = rateMovieMutation({
+      const ratingResponse = await rateMovieMutation({
         id: movieId,
         rating: ratingValue,
         sessionId,
       });
 
-      if (ratingResponse.status.success) setRating(ratingValue);
+      if (ratingResponse.success) setRating(ratingValue);
     }
   };
 
