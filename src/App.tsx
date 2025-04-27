317a1b1135649d5e186871c5e9d9896820b2b13d
Puzant <pbakjejian@hotmail.com>
2025-04-27 10:50:30 +0300

    wrap the app in suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import React from "react";
+import "@/i18n";
 import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
+import React, { Suspense } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import LanguageDetector from "i18next-browser-languagedetector";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -12,38 +11,21 @@ import { AdapterLuxon } from "@mui/x-date-pickers/AdapterLuxon";
 
 import AppRoutes from "@/appRoutes";
 import { Navbar } from "@/components/organisms";
+import useUserStore from "@/store/useUserStore";
 import ErrorBoundry from "./errorBoundry";
 import UseScrollToTop from "./hooks/useScrollToTop";
 
-import en from "@/locale/en.json";
-import fr from "@/locale/fr.json";
-import ar from "@/locale/ar.json";
-
 function App() {
   const queryClient = new QueryClient();
-
-  i18n
-    .use(initReactI18next)
-    .use(LanguageDetector)
-    .init({
-      detection: {
-        order: ["localStorage", "navigator"],
-      },
-      resources: {
-        en: {
-          translation: en,
-        },
-        fr: {
-          translation: fr,
-        },
-        ar: {
-          translation: ar,
-        },
-      },
-      debug: false,
-    });
+  const { fontStyle } = useUserStore();
 
   React.useEffect(() => {
+    if (fontStyle === "italic") {
+      document.body.style.fontStyle = "italic";
+    } else if (fontStyle === "normal") {
+      document.body.style.fontStyle = "normal";
+    }
+
     const handleDir = () => {
       const direction = i18n.dir(i18n.language);
       document.documentElement.dir = direction;
@@ -55,21 +37,23 @@ function App() {
     return () => {
       i18n.off("languageChanged", handleDir);
     };
-  }, []);
+  }, [fontStyle]);
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <LocalizationProvider dateAdapter={AdapterLuxon} adapterLocale="de">
-        <BrowserRouter>
-          <UseScrollToTop />
-          <Navbar />
-          <ToastContainer hideProgressBar={false} theme="dark" />
-          <ErrorBoundry>
-            <AppRoutes />
-          </ErrorBoundry>
-        </BrowserRouter>
-      </LocalizationProvider>
-    </QueryClientProvider>
+    <Suspense fallback={<div>Loading page...</div>}>
+      <QueryClientProvider client={queryClient}>
+        <LocalizationProvider dateAdapter={AdapterLuxon} adapterLocale="de">
+          <BrowserRouter>
+            <UseScrollToTop />
+            <Navbar />
+            <ToastContainer hideProgressBar={false} theme="dark" />
+            <ErrorBoundry>
+              <AppRoutes />
+            </ErrorBoundry>
+          </BrowserRouter>
+        </LocalizationProvider>
+      </QueryClientProvider>
+    </Suspense>
   );
 }
 
