2083022a11f534ba46bd712bc976f49e95b65e4b
Puzant <pbakjejian@hotmail.com>
2025-04-29 11:58:23 +0300

    update hooks

--- src/hooks/useMovieDetails.ts
+++ src/hooks/useMovieDetails.ts
@@ -1,6 +1,5 @@
-import React from "react";
+import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
-import { extractColors } from "extract-colors";
 import { useParams } from "react-router-dom";
 
 import useMovieDetailsQuery from "@/queries/useMovieDetailsQuery";
@@ -14,73 +13,23 @@ const useMovieDetails = (apiFunctions: IApiFunction) => {
   const { setMovieStatus } = useMovieStore();
   const { sessionId, isAuthenticated } = useUserStore();
 
-  const [posterBackDropColors, setPosterBackDropColors] = React.useState<any>([]);
+  const { data: movieDetails, isLoading, error } = useMovieDetailsQuery(apiFunctions, movieId, i18n, sessionId);
 
-  const {
-    data: movieDetails,
-    isLoading,
-    error,
-  } = useMovieDetailsQuery(apiFunctions, movieId, i18n, sessionId);
-
-  React.useEffect(() => {
+  useEffect(() => {
     const movieStatus = movieDetails?.account_states;
 
     if (isAuthenticated && movieStatus) {
       setMovieStatus(movieStatus);
     }
-  }, [isAuthenticated, movieDetails]);
-
-  React.useEffect(() => {
-    const fetchColor = async () => {
-      try {
-        if (movieDetails) {
-          const posterPath = `https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/${movieDetails.backdrop_path}`;
-          const colors = await extractColors(posterPath, options);
-          setPosterBackDropColors(colors);
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchColor();
-  }, [movieDetails]);
-
-  const options = {
-    pixels: 64000,
-    distance: 0.22,
-    crossOrigin: "Anonymous",
-    saturationDistance: 0.2,
-    lightnessDistance: 0.2,
-    hueDistance: 0.083333333,
-  } as {};
-
-  React.useEffect(() => {
-    const fetchColor = async () => {
-      try {
-        if (movieDetails) {
-          const posterPath = `https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/${movieDetails.backdrop_path}`;
-          const colors = await extractColors(posterPath, options);
-          setPosterBackDropColors(colors);
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchColor();
-  }, [movieDetails]);
+  }, [isAuthenticated, movieDetails, setMovieStatus]);
 
   return {
     i18n,
     t,
     movieId,
-    posterBackDropColors,
-    setPosterBackDropColors,
     movieDetails,
     isLoading,
     error,
-    options,
   };
 };
 
--- src/store/useMovieStore.ts
+++ src/store/useMovieStore.ts
@@ -1,17 +1,17 @@
-import { create } from 'zustand';
+import { create } from "zustand";
 
 interface IMovieStatus {
-  favorite: boolean
-  watchlist: boolean
-  rated: boolean | {value: number}
+  favorite: boolean;
+  watchlist: boolean;
+  rated: boolean | { value: number };
 }
 
 interface IMovieStore {
-  isFavorite: boolean
-  isInWatchlist: boolean
-  isRated: number | boolean | null
-  setMovieStatus: (param: IMovieStatus) => void
-  resetMovieStatus: () => void
+  isFavorite: boolean;
+  isInWatchlist: boolean;
+  isRated: number | boolean | null;
+  setMovieStatus: (param: IMovieStatus) => void;
+  resetMovieStatus: () => void;
 }
 
 const useMovieStore = create<IMovieStore>((set) => ({
@@ -19,17 +19,19 @@ const useMovieStore = create<IMovieStore>((set) => ({
   isInWatchlist: false,
   isRated: null,
 
-  setMovieStatus: (movieStatus) => set({ 
-    isFavorite: movieStatus.favorite, 
-    isInWatchlist: movieStatus.watchlist, 
-    isRated: typeof movieStatus.rated === 'boolean' ?  movieStatus.rated : movieStatus.rated.value
-  }),
+  setMovieStatus: (movieStatus) =>
+    set({
+      isFavorite: movieStatus.favorite,
+      isInWatchlist: movieStatus.watchlist,
+      isRated: typeof movieStatus.rated === "object" ? movieStatus.rated.value : movieStatus.rated,
+    }),
 
-  resetMovieStatus: () => set({
-    isFavorite: false,
-    isInWatchlist: false,
-    isRated: null,
-  })
-}))
+  resetMovieStatus: () =>
+    set({
+      isFavorite: false,
+      isInWatchlist: false,
+      isRated: null,
+    }),
+}));
 
-export default useMovieStore
+export default useMovieStore;
