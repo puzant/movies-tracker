601a0728886ff36ea6a5096324b47a2b876df32c
Puzant <pbakjejian@hotmail.com>
2025-04-30 15:02:41 +0300

    css updates

--- src/components/molecules/movie/movie.tsx
+++ src/components/molecules/movie/movie.tsx
@@ -14,28 +14,24 @@ export const Movie = ({ movie }: { movie: IMovie }) => {
         />
       );
 
-    return (
-      <img
-        src={moviePlacegolder}
-        alt="Movie Poster Placeholder"
-        className="bg-[#dbdbdb] rounded-t-xl"
-      />
-    );
+    return <img src={moviePlacegolder} alt="Movie Poster Placeholder" className="bg-[#dbdbdb] rounded-t-xl" />;
   };
 
   return (
     <div className="h-full border rounded-xl flex flex-col shadow-md cursor-pointer">
       {getMoviePoster()}
 
-      <div className="p-3 flex flex-col">
-        <span className="font-bold">{movie.title}</span>
-        <span className="text-gray-500">
-          {DateTime.fromISO(movie.release_date).toLocaleString({
-            month: "long",
-            day: "2-digit",
-            year: "numeric",
-          })}
-        </span>
+      <div className="p-3 flex flex-col justify-between h-full">
+        <div className="flex flex-col gap-[1px]">
+          <span className="font-bold">{movie.title}</span>
+          <span className="text-gray-500">
+            {DateTime.fromISO(movie.release_date).toLocaleString({
+              month: "long",
+              day: "2-digit",
+              year: "numeric",
+            })}
+          </span>
+        </div>
         <MovieRating vote={movie.vote_average} />
       </div>
     </div>
