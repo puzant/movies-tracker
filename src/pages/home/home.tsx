e99e8362ed83a4ae7a0293a29cb014fc25fe1bdf
Puzant <pbakjejian@hotmail.com>
2025-04-30 16:34:22 +0300

    apply proper semantic to home component

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -25,27 +25,27 @@ const Home = ({ apiFunctions }: { apiFunctions: IApiFunction }) => {
 
   return (
     <div className="mt-8">
-      <div className="border shadow-md rounded-md w-[90%] md:w-[50%] p-2 md:p-5 flex flex-col md:flex-row gap-4 items-center justify-between m-auto my-6 md:my-0">
+      <figure className="border shadow-md rounded-md w-[90%] md:w-[50%] p-2 md:p-5 flex flex-col md:flex-row gap-4 items-center justify-between m-auto my-6 md:my-0">
         <img className="w-[80px] h-[40px] md:w-[110px] md:h-[52px]" src={tmdbLogo} alt="tmdb logo" />
-        <div className="text-sm text-gray-500 text-center">{t("tmdb_notice")}</div>
-      </div>
+        <figcaption className="text-sm text-gray-500 text-center">{t("tmdb_notice")}</figcaption>
+      </figure>
 
-      <div className="px-4 sm:px-8 flex justify-between items-center">
-        <span className="text-2xl font-semibold">{t("popular_movies")}</span>
-        <div className="block md:hidden" onClick={() => setOpenDialog(true)}>
+      <header className="px-4 sm:px-8 flex justify-between items-center">
+        <h2 className="text-2xl font-semibold">{t("popular_movies")}</h2>
+        <button className="block md:hidden" onClick={() => setOpenDialog(true)}>
           <FilterListIcon />
-        </div>
-      </div>
+        </button>
+      </header>
 
-      <div className="flex gap-6 px-4 sm:px-8 py-4">
-        <div className="w-[30%] lg:w-[20%] hidden sm:block">
+      <main className="flex gap-6 px-4 sm:px-8 py-4">
+        <aside className="w-[30%] lg:w-[20%] hidden sm:block">
           <Filters />
-        </div>
+        </aside>
 
         {error ? (
           <ErrorMessage />
         ) : (
-          <div className="w-full md:w-[70%] lg:w-[80%]">
+          <section className="w-full md:w-[70%] lg:w-[80%]">
             {status === "pending" ? (
               <div className="flex justify-center">
                 <LoadingSpinner />
@@ -53,14 +53,16 @@ const Home = ({ apiFunctions }: { apiFunctions: IApiFunction }) => {
             ) : (
               <>
                 {moviesData?.pages.every((p: IPage) => p.results.length === 0) ? (
-                  <div className="text-3xl text-center">{t("no_results")}</div>
+                  <p className="text-3xl text-center">{t("no_results")}</p>
                 ) : (
                   <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-7">
                     {moviesData?.pages.map((page: IPage) =>
                       page.results.map((movie: IMovie) => (
-                        <Link key={movie.id} to={`/movie/${movie.id}`}>
-                          <Movie movie={movie} />
-                        </Link>
+                        <article key={movie.id}>
+                          <Link to={`/movie/${movie.id}`}>
+                            <Movie movie={movie} />
+                          </Link>
+                        </article>
                       ))
                     )}
                   </div>
@@ -79,9 +81,9 @@ const Home = ({ apiFunctions }: { apiFunctions: IApiFunction }) => {
                 </Button>
               )}
             </div>
-          </div>
+          </section>
         )}
-      </div>
+      </main>
 
       <FiltersDialog onClose={() => setOpenDialog(false)} openDialog={openDialog} />
     </div>
