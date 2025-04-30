0f9d5d9855b4f3b80b9d716f46591e15a78890b4
Puzant <pbakjejian@hotmail.com>
2025-04-30 15:03:03 +0300

    use i18n Backend package in i18n config

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,12 +1,10 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 
-import en from "@/locale/en.json";
-import fr from "@/locale/fr.json";
-import ar from "@/locale/ar.json";
-
 i18n
+  .use(Backend)
   .use(initReactI18next)
   .use(LanguageDetector)
   .init({
@@ -14,16 +12,8 @@ i18n
     detection: {
       order: ["localStorage", "navigator"],
     },
-    resources: {
-      en: {
-        translation: en,
-      },
-      fr: {
-        translation: fr,
-      },
-      ar: {
-        translation: ar,
-      },
+    backend: {
+      loadPath: "/locale/{{lng}}.json",
     },
     debug: false,
   });
