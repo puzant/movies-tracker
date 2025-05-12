be907308c61e9ba050ca77e57723bae592e4e24b
Puzant <pbakjejian@hotmail.com>
2025-05-12 15:54:54 +0300

    update types

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,16 +1,15 @@
 import * as yup from "yup";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-
-import useRequestTokenQuery from "@/queries/useRequestTokenQuery";
-import useLoginMutation from "@/mutations/useLoginMutation";
-import useCreateSessionMutation from "@/mutations/useCreateSessionMutation";
+import { useQuery, useMutation } from "@tanstack/react-query";
 
 import useUserStore from "@/store/useUserStore";
+import { IApiFunction } from "@/interfaces";
 
 interface ILoginPayload {
   username: string;
   password: string;
+  requestToken?: string;
 }
 
 const loginSchema = yup.object().shape({
@@ -18,15 +17,28 @@ const loginSchema = yup.object().shape({
   password: yup.string().required("Password field is required"),
 });
 
-const useLogin = () => {
+const useLogin = (apiFunctions: IApiFunction) => {
   const { t } = useTranslation();
   const navigate = useNavigate();
 
   const { accentColor } = useUserStore();
-  const { data: requestToken } = useRequestTokenQuery();
 
-  const { mutateAsync: loginMutation, isPending: loginLoading, error } = useLoginMutation();
-  const { mutateAsync: createSessionMutation } = useCreateSessionMutation();
+  const { data: requestToken } = useQuery({
+    queryKey: [apiFunctions.getRequestToken.key],
+    queryFn: () => apiFunctions.getRequestToken.func(),
+  });
+
+  const {
+    mutateAsync: loginMutation,
+    isPending: loginLoading,
+    error,
+  } = useMutation({
+    mutationFn: (payload: ILoginPayload) => apiFunctions.login.func(payload),
+  });
+
+  const { mutateAsync: createSessionMutation } = useMutation({
+    mutationFn: (payload: { requestToken: string }) => apiFunctions.createSession.func(payload),
+  });
 
   const handleLogin = async (payload: ILoginPayload) => {
     try {
