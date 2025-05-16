b56c5bac8782c9507526c7207373a4b1b4628bdc
Puzant <pbakjejian@hotmail.com>
2025-05-16 18:58:13 +0300

    run sentry in production only

--- src/main.tsx
+++ src/main.tsx
@@ -5,20 +5,22 @@ import * as Sentry from "@sentry/react";
 import App from "./App.tsx";
 import "./index.css";
 
-Sentry.init({
-  dsn: "https://cfa61d1d5ec1abd97e2ce5f957e4a8d7@o4506722166374400.ingest.sentry.io/4506722184790016",
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration({
-      maskAllText: false,
-      blockAllMedia: false,
-    }),
-  ],
-  tracesSampleRate: 1.0,
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-});
+if (import.meta.env.MODE === "production") {
+  Sentry.init({
+    dsn: "https://cfa61d1d5ec1abd97e2ce5f957e4a8d7@o4506722166374400.ingest.sentry.io/4506722184790016",
+    integrations: [
+      Sentry.browserTracingIntegration(),
+      Sentry.replayIntegration({
+        maskAllText: false,
+        blockAllMedia: false,
+      }),
+    ],
+    tracesSampleRate: 1.0,
+    tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+  });
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
